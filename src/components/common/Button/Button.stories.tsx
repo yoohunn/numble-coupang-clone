743ab568029e63d38d227474ffb4149f985c60a9
YooHyunJi <>
2022-07-07 20:56:39 +0900

    chore(): move stories

--- src/components/common/Button/Button.stories.tsx
+++ src/components/common/Button/Button.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-import Button from '../components/common/Button';
+import Button from './Button';
 
 export default {
   title: 'Button',
