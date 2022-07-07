82d93b1cfb6f8d18058b4a947611e09e82b1299d
YooHyunJi <>
2022-07-07 20:15:17 +0900

    feat(): add Input.stories

---
+++ src/stories/Input.stories.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import Input from '../components/common/Input';
+
+export default {
+  title: 'Input',
+  component: Input,
+} as ComponentMeta<typeof Input>;
+
+const Template: ComponentStory<typeof Input> = (args) => <Input {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  placeholder: 'default input',
+};
+
+export const Email = Template.bind({});
+Email.args = {
+  type: 'email',
+  label: '📧',
+  placeholder: '아이디(이메일)',
+  message: '',
+};
+
+export const Password = Template.bind({});
+Password.args = {
+  type: 'password',
+  label: '🔒',
+  placeholder: '비밀번호',
+};
+
+export const Error = Template.bind({});
+Error.args = {
+  type: 'password',
+  label: '🔒',
+  placeholder: '비밀번호',
+  message: '비밀번호 에러',
+};
