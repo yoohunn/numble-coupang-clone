9606f7407ad31faa90cfa4b6f278b21c0f22e3db
YooHyunJ <poirtmove@gmail.com>
2022-07-14 22:18:06 +0900

    enhance(): update Input

--- src/components/common/Input/Input.stories.tsx
+++ src/components/common/Input/Input.stories.tsx
@@ -6,34 +6,66 @@ import Input from './Input';
 export default {
   title: 'Input',
   component: Input,
+  decorators: [
+    (Story) => (
+      <div style={{ margin: 'auto', minWidth: '290px', maxWidth: '460px' }}>
+        <Story />
+      </div>
+    ),
+  ],
 } as ComponentMeta<typeof Input>;
 
 const Template: ComponentStory<typeof Input> = (args) => <Input {...args} />;
 
-export const Default = Template.bind({});
-Default.args = {
-  placeholder: 'default input',
+export const Standard = Template.bind({});
+Standard.args = {
+  placeholder: 'standard input',
 };
 
-export const Email = Template.bind({});
-Email.args = {
+export const Warning = Template.bind({});
+Warning.args = {
   type: 'email',
-  label: '📧',
+  icon: '📧',
   placeholder: '아이디(이메일)',
-  message: '',
+  message: '이메일을 입력하세요',
 };
 
-export const Password = Template.bind({});
-Password.args = {
+export const Valid = Template.bind({});
+Valid.args = {
+  icon: '🔒',
   type: 'password',
-  label: '🔒',
   placeholder: '비밀번호',
+  isValid: true,
+  validations: [
+    {
+      type: 'pattern',
+      invalid: false,
+      message: '영문/숫자/특수문자 2가지 이상 조합 (8~20자)',
+    },
+    {
+      type: 'minLength',
+      invalid: false,
+      message: '비밀번호는 8자 이상이여야 합니다.',
+    },
+  ],
 };
 
-export const Error = Template.bind({});
-Error.args = {
+export const Invalid = Template.bind({});
+Invalid.args = {
+  icon: '🔒',
   type: 'password',
-  label: '🔒',
   placeholder: '비밀번호',
   message: '비밀번호 에러',
+  validations: [
+    {
+      type: 'pattern',
+      invalid: true,
+      message: '영문/숫자/특수문자 2가지 이상 조합 (8~20자)',
+    },
+    {
+      type: 'minLength',
+      invalid: false,
+      message: '비밀번호는 8자 이상이여야 합니다.',
+    },
+  ],
 };
--- src/components/common/Input/Input.tsx
+++ src/components/common/Input/Input.tsx
@@ -1,71 +1,116 @@
-import { HTMLInputTypeAttribute, InputHTMLAttributes, forwardRef } from 'react';
+import { InputHTMLAttributes, forwardRef, ComponentProps } from 'react';
 import styled from '@emotion/styled';
 
-interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+interface IIput extends InputHTMLAttributes<HTMLInputElement>, IMessage {
+  icon?: string;
   placeholder: string;
-  label?: string;
-  message?: string;
+  isValid?: boolean;
 }
 
-//TODO: valid 할 때 focuse 가 사라지면 span 추가하기 valid
-const Input = forwardRef<HTMLInputElement, InputProps>(
-  ({ type, label, message, ...rest }, ref) => (
-    <>
-      <Label error={!!message}>
-        <span>{label || '💌'}</span>
-        <input type={type} {...rest} ref={ref} />
-      </Label>
-      {message && <Span>{message}</Span>}
-    </>
+const Input = forwardRef<HTMLInputElement, IIput>(
+  ({ type, icon, message, isValid, validations, ...props }, ref) => (
+    <Wrapper error={!!message}>
+      <label>
+        <span>{icon || '✉️'}</span>
+        <input type={type} ref={ref} {...props} />
+        {isValid && <span className='valid-icon'>✓</span>}
+      </label>
+      <Messages message={message} validations={validations} />
+    </Wrapper>
   )
 );
 
 export default Input;
 
-const Label = styled.label<{ error: boolean }>`
-  display: flex;
-  margin-top: 14px;
-  width: 100%;
-  border: 1px solid #ccc;
-  background-color: #fafafa;
-  transition: border-color 0.25s ease;
-
-  border-bottom: ${({ error }) => (error ? '2px solid #e7223d' : '')};
-
+const Wrapper = styled.label<{ error: boolean }>`
   &:focus-within {
-    border-bottom: 2px solid ${({ error }) => (error ? '#e7223d' : '#0074e9')};
+    border: red;
   }
-
-  span {
-    min-width: 44px;
-    height: 100%;
-    text-align: center;
-    margin: auto;
-  }
-
-  input {
-    border: 0;
-    height: 48px;
+  & > label {
+    display: flex;
+    position: relative;
+    margin-top: 14px;
     width: 100%;
-    padding: 16px 12px;
-    border-left: 1px solid #ccc;
-    color: #111;
-    font-size: 14px;
-    line-height: 20px;
-    font-weight: 700;
+    border: 1px solid #ccc;
+    background-color: #fafafa;
+    transition: border-color 0.25s ease;
+    border-bottom: ${({ error }) => (error ? '2px solid #e7223d' : '')};
 
-    &:focus {
-      outline: none;
+    &:focus-within {
+      border-bottom: 2px solid ${({ error }) => (error ? '#e7223d' : '#0074e9')};
     }
-    &::placeholder {
-      color: #aaa;
+
+    & > span {
+      margin: auto;
+      min-width: 44px;
+      height: 100%;
+      text-align: center;
+    }
+
+    input {
+      padding: 16px 12px;
+      height: 48px;
+      width: 100%;
+      border-left: 1px solid #ccc;
+      color: #111;
+      font-size: 14px;
+      font-weight: 700;
+      line-height: 20px;
+
+      &:focus {
+        outline: none;
+      }
+      &::placeholder {
+        color: #aaa;
+      }
+    }
+    .valid-icon {
+      position: absolute;
+      right: 0;
+      top: 13px;
+      color: #0074e9;
     }
   }
 `;
 
-const Span = styled.div`
-  color: #e52528;
+interface IMessage {
+  message?: string;
+  validations?: { type: string; invalid: boolean; message: string }[];
+}
+
+const Messages = ({ message, validations }: IMessage) => (
+  <Box>
+    {validations
+      ? validations.map(({ invalid, message }) => (
+          <ValidMessages invalid={invalid}>
+            <span>{invalid ? 'X' : '✓'}</span>
+            {message}
+          </ValidMessages>
+        ))
+      : message && <span>{message}</span>}
+  </Box>
+);
+
+const Box = styled.div`
   margin: 9px 12px 0;
+
   font-size: 12px;
   line-height: 18px;
+  color: #e52528;
+  span {
+  }
+`;
+
+const ValidMessages = styled.span<{ invalid: boolean }>`
+  display: block;
+  font-size: 12px;
+  line-height: 18px;
+  color: ${(props) => (props.invalid ? '#e52528' : '#00891a')};
+
+  span {
+    display: inline-block;
+    width: 16px;
+    height: 18px;
+    color: ${(props) => (props.invalid ? '#e52528' : '#00891a')};
+  }
 `;
