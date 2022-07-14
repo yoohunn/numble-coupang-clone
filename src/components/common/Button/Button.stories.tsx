46a16fa491fac72e2e4184ab07dead6e1338559a
YooHyunJ <poirtmove@gmail.com>
2022-07-14 22:18:28 +0900

    enhance(): update Button

--- src/components/common/Button/Button.stories.tsx
+++ src/components/common/Button/Button.stories.tsx
@@ -6,30 +6,37 @@ import Button from './Button';
 export default {
   title: 'Button',
   component: Button,
+  decorators: [
+    (Story) => (
+      <div style={{ margin: 'auto', minWidth: '290px', maxWidth: '460px' }}>
+        <Story />
+      </div>
+    ),
+  ],
 } as ComponentMeta<typeof Button>;
 
 const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 
-export const Default = Template.bind({});
-Default.args = {
-  title: 'default',
+export const Standard = Template.bind({});
+Standard.args = {
+  title: 'Standard',
 };
 
-export const Login = Template.bind({});
-Login.args = {
+export const NomalButton = Template.bind({});
+NomalButton.args = {
   title: '로그인',
   type: 'button',
   colored: true,
 };
 
-export const SignIn = Template.bind({});
-SignIn.args = {
+export const Link = Template.bind({});
+Link.args = {
   title: '회원가입',
-  href: '/signin',
+  to: '/signin',
 };
 
-export const Agreement = Template.bind({});
-Agreement.args = {
+export const Submit = Template.bind({});
+Submit.args = {
   title: '동의하고 가입하기',
   type: 'submit',
   colored: true,
