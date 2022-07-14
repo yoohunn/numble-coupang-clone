f7d8ec4be2df7c198b7e26a7fca84ea0a99163ac
YooHyunJ <poirtmove@gmail.com>
2022-07-14 22:18:28 +0900

    enhance(): update Button

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -2,45 +2,55 @@ import React from 'react';
 import Link from 'next/link';
 import styled from '@emotion/styled';
 
-interface IButton {
+type Props = {
   title: string;
   onClick?: () => {};
-  href?: string;
-  type?: 'button' | 'submit';
+  to?: string;
   colored?: boolean;
-}
+  type?: 'button' | 'submit';
+};
 
-const Button = ({ href, type, title, colored, onClick }: IButton) =>
-  href ? (
-    <Link href={href} passHref>
-      <A colored={colored}>{title}</A>
-    </Link>
-  ) : (
-    <StyledButton colored={colored} type={type} onClick={onClick}>
-      {title}
-    </StyledButton>
-  );
+const Button = ({ to, type, title, colored, onClick }: Props) => (
+  <Wrapper colored={colored}>
+    {to ? (
+      <Link href={to} passHref>
+        <a>{title}</a>
+      </Link>
+    ) : (
+      <button type={type} onClick={onClick}>
+        {title}
+      </button>
+    )}
+  </Wrapper>
+);
 
 export default Button;
 
-export const StyledButton = styled.button<{ colored?: boolean }>`
-  outline: none;
-  display: block;
-  width: 100%;
-  padding: 17px 0;
-  color: ${({ colored }) => (colored ? '#fff' : '#0074e9')};
-  background-color: ${({ colored }) => (colored ? '#0074e9' : '#fff')};
-  box-shadow: inset 0 -1px 0 ${({ colored }) => (colored ? 'rgba(0, 0, 0, 0.38)' : 'rgba(0, 0, 0, 0.15)')};
-  border: ${({ colored }) => (colored ? 'none' : '1px solid #ccc')};
-  border-radius: 2px;
-  font-size: 17px;
-  line-height: 20px;
-  font-weight: 700;
-  text-align: center;
+export const Wrapper = styled.div<{ colored?: boolean }>`
+  button,
+  a {
+    display: block;
+    width: 100%;
+    padding: 17px 0;
+    border-radius: 2px;
+    font-size: 17px;
+    line-height: 20px;
+    font-weight: 700;
+    text-align: center;
+    color: ${({ colored }) => (colored ? '#fff' : '#0074e9')};
+    background-color: ${({ colored }) => (colored ? '#0074e9' : '#fff')};
+    box-shadow: inset 0 -1px 0 ${({ colored }) => (colored ? 'rgba(0, 0, 0, 0.38)' : 'rgba(0, 0, 0, 0.15)')};
+    border: ${({ colored }) => (colored ? 'none' : '1px solid #ccc')};
 
-  &:hover {
-    cursor: pointer;
+    &:hover {
+      cursor: pointer;
+    }
+
+    &:active {
+      box-shadow: ${({ colored }) =>
+        colored
+          ? 'inset 0 2px 0 rgba(0,0,0,0.38);'
+          : 'inset 0 1px 0 rgba(0, 0, 0, 0.15);'};
+    }
   }
 `;
-
-export const A = StyledButton.withComponent('a');
