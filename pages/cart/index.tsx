6653d89085d5f84cf5820445cc99777fe6cfaf1f
YooHyunJ <poirtmove@gmail.com>
2022-09-06 02:19:32 +0900

    feat(): add CartPage

--- pages/cart/index.tsx
+++ pages/cart/index.tsx
@@ -1,3 +1,31 @@
+import { useState } from 'react';
+import styled from '@emotion/styled';
+
 export default function CartPage() {
-  return <>Cart</>;
+  // 서버에서 카드 아이템 가져오기
+  // const { cartItems } = useGetCart()
+
+  // 클라이언트에서 이벤트를 받은 후 서버에 전달
+  const [seletedItems, setSeletedItems] = useState();
+
+  const select = (id: number) => {};
+  const selectAll = () => {};
+
+  const remove = (id: number) => {};
+  const removeAll = () => {};
+
+  return (
+    <Grid>
+      {/* Head */}
+      {/* Order title='rocket'*/}
+      {/*   title */}
+      {/*   CartItems */}
+      {/*   totalPrice */}
+      {/* Order title='seller' */}
+      {/* Foot */}
+      {/* TotalOrder */}
+    </Grid>
+  );
 }
+
+const Grid = styled.div``;
